9920e495b7a11143b6c64861d30d3eccd113de40
gpires97 <162461724+gpires97@users.noreply.github.com>
2024-04-03 07:27:45 -0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,27 @@
+import os
+
+print("""Sabor Express
+      """)
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+#https://fsymbols.com/pt/letras/
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+#print('Você escolheu a opção', opcao_escolhida)
+#print(f'Você escolheu a opção {opcao_escolhida}')
+
+def finaliza_app():
+    os.system('cls')
+    print('Encerrando o programa\n')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+     finaliza_app()
